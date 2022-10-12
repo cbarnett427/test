34a33ef01a2a7ecda9ce7e358766a3ff0710765e
Clayton Barnett <Cbarnett427@live.com>
2022-10-12 15:27:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+
+// ---------- REVEAL ELEMENTS ON SCROLL ----------
+function reveal() {
+	var reveals = document.querySelectorAll(".reveal");
+	for(var i = 0; i < reveals.length; i++) {
+		var windowHeight = window.innerHeight;
+		var elementTop = reveals[i].getBoundingClientRect().top;
+		var elementVisible = 100;
+		if(elementTop < windowHeight - elementVisible) {
+			reveals[i].classList.add("active");
+		} else {
+			reveals[i].classList.remove("active");
+		}
+	}
+}
+window.addEventListener("scroll", reveal);
+// To check the scroll position on page load
+reveal();
